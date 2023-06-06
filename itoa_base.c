f7842d48dc89e510bec5be03e8d97c490a433f26
rscres <rseelaen@student.42sp.org.br>
2023-06-06 18:22:57 -0300

    add working itoa_base

--- itoa_base.c
+++ itoa_base.c
@@ -6,11 +6,12 @@
 /*   By: rseelaen <rseelaen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/02 21:11:37 by rseelaen          #+#    #+#             */
-/*   Updated: 2023/06/06 16:04:11 by rseelaen         ###   ########.fr       */
+/*   Updated: 2023/06/06 17:47:22 by rseelaen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+#include <stdio.h>
 
 static int	intlen(int n, int base)
 {	
@@ -32,30 +33,56 @@ static int	intlen(int n, int base)
 
 static char	*strwriter(char *str, int len, int n, int base)
 {
-	int	minus;
+	int		minus;
+	char	*digits;
 
+	digits = "0123456789abcdef";
 	minus = 0;
 	if (n < 0)
 		minus = 1;
 	while (len-- > minus)
 	{
 		if (n % base < 0)
-			str[len] = -(n % base) + '0';
+			str[len] = digits[-(n % base)];
 		else
-			str[len] = n % base + '0';
-		n = n / 10;
+			str[len] = digits[n % base];
+		n = n / base;
 	}
 	return (str);
 }
 
-char	*ft_itoa(int n, int base)
+static int	ft_isalpha(int a)
+{
+	if ((a >= 65 && a <= 90) || (a >= 97 && a <= 122))
+		return (1);
+	return (0);
+}
+
+
+static char	*hex_case(char *str)
+{
+	int		i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - 32;
+		i++;
+	}
+	return (str);
+}
+
+char	*ft_itoa_base(unsigned int n, int base, int caps)
 {
 	char	*str;
 	int		len;
 	int		minus;
 
 	minus = 0;
-	len = intlen(n);
+	if (base != 16 && base != 10 && base != 8 && base != 2)
+		return (0);
+	len = intlen(n, base);
 	if (n < 0)
 		len = len + 1;
 	str = malloc((len + 1) * sizeof(char));
@@ -67,12 +94,7 @@ char	*ft_itoa(int n, int base)
 		str[0] = '-';
 	str[len] = '\0';
 	strwriter(str, len, n, base);
+	if (caps)
+		hex_case(str);
 	return (str);
-}
-
-int main()
-{
-	
-
-	return (0);
 }
